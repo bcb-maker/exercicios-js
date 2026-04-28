3fb3a74af9e6482b2c3b5bb0609228921ce71c7c
bcb-maker <bcb@aluno.ifnmg.edu.br>
2026-04-28 16:55:21 +0000

    feat: Exercicio 8

--- exercicios/aula08/utils/matematica.js
+++ exercicios/aula08/utils/matematica.js
@@ -5,17 +5,17 @@
 // Todas recebem dois números (a, b) e retornam o resultado da operação.
 
 export function somar(a, b) {
-  // escreva seu código aqui
+  return a + b
 }
 
 export function subtrair(a, b) {
-  // escreva seu código aqui
+  return a - b
 }
 
 export function multiplicar(a, b) {
-  // escreva seu código aqui
+  return a * b
 }
 
 export function dividir(a, b) {
-  // escreva seu código aqui
+  return a / b
 }
